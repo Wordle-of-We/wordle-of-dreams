e4172f337744385d2f0d8935197207669e0b376a
avelando <avelarrodrigues89@gmail.com>
2025-08-11 01:56:20 -0300

    Corrigindo o front end para usar o refresh token

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -2,17 +2,19 @@ import api from '../lib/api';
 import type { User } from '../interfaces/User';
 
 export interface AuthResponse {
-  token: string;
+  accessToken: string;
+  refreshToken: string;
   user: User;
 }
 
 export const authService = {
   async login(email: string, password: string): Promise<AuthResponse> {
-    const { data } = await api.post<AuthResponse>(
-      '/auth/login',
-      { email, password }
-    );
-    localStorage.setItem('adminToken', data.token);
+    const { data } = await api.post<AuthResponse>('/auth/login-admin', { email, password });
+
+    localStorage.setItem('adminToken', data.accessToken);
+    localStorage.setItem('adminRefreshToken', data.refreshToken);
+    localStorage.setItem('adminEmail', email);
+
     return data;
   },
 
@@ -23,6 +25,8 @@ export const authService = {
 
   logout() {
     localStorage.removeItem('adminToken');
+    localStorage.removeItem('adminRefreshToken');
+    localStorage.removeItem('adminEmail');
     window.location.href = '/loginAdmin';
   },
 };
